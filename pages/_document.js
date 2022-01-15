9e7e71d1ccbbd5cabc52cbe75d0e85631c4087d7
zuyo <shield41791@gmail.com>
2022-01-16 00:01:14 +0900

    add baseuri

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="ko">
-                <Head />
+                <Head>
+                    <base href="{{ precess.env.baseUri }}"></base>
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
