4d985fb6034d37f178a94675a806fa426caf7c80
zuyo <shield41791@gmail.com>
2022-01-18 15:50:15 +0900

    no title,desc test

--- pages/contact.js
+++ pages/contact.js
@@ -4,9 +4,9 @@ export default function contact({ imageUrl } ) {
     return <>
         <Head>
             <meta name="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:title" content="Dior"/>
+            {/* <meta name="twitter:title" content="Dior"/>
             <meta name="twitter:description"
-            content="This is contact page"/>
+            content="This is contact page"/> */}
             <meta name="twitter:image"
                 content={ imageUrl }/>
         </Head>
