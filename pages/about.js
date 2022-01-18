6926f24bb03b336b2a9c3440dfbc42ede0f1eae5
zuyo <shield41791@gmail.com>
2022-01-18 15:12:48 +0900

    fix metatag

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,9 @@ export default function About({ imageUrl }) {
         <>
             <Head>
                 <meta name="twitter:card" content="summary_large_image"/>
-                <meta name="twitter:site" content="@nytimes"/>
-                <meta name="twitter:creator" content="@SarahMaslinNir"/>
-                <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral"/>
+                <meta name="twitter:title" content="Dior"/>
                 <meta name="twitter:description"
-                content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."/>
+                content="Welcome to Dior"/>
                 <meta name="twitter:image"
                     content={ imageUrl }/>
             </Head>
