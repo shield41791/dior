36ba8de6d81bfb81b4c5c9ec09a4668c694ed6e9
zuyo <shield41791@gmail.com>
2022-01-18 12:25:04 +0900

    fix dynamic meta

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,14 @@ export default function About() {
         // <div>pages 폴더 아래에 생성하면, 별다른 설정없이 라우팅된다.</div>;
         <>
             <Head>
-                <meta property="og:image" content={process.env.logoPath} />
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:site" content="@nytimes"/>
+                <meta name="twitter:creator" content="@SarahMaslinNir"/>
+                <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral"/>
+                <meta name="twitter:description"
+                content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."/>
+                <meta name="twitter:image"
+                    content={ process.env.logoPath }/>
             </Head>
             <div>
                 <Header as="h3" style={{ paddingTop: 40 }} color="teal">
--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function contact(params) {
-    return <>contact</>
+    return <>
+        <Head>
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:site" content="@nytimes"/>
+            <meta name="twitter:creator" content="@SarahMaslinNir"/>
+            <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral"/>
+            <meta name="twitter:description"
+            content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."/>
+            <meta name="twitter:image"
+                content={ process.env.logoPathTest }/>
+        </Head>
+        contact
+    </>
 }
