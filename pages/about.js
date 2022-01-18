3b153361943297c4c394bc97d0a944156acc6def
zuyo <shield41791@gmail.com>
2022-01-18 12:06:09 +0900

    dynamic meta tag

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
 import { Button, Divider, Form, FormButton, Header, Input, List, TextArea } from "semantic-ui-react";
 
 export default function About() {
+    document.querySelector('meta[name="twitter:image"]')?.setAttribute('content', process.env.logoPath)
+    
     return (
         // <div>pages 폴더 아래에 생성하면, 별다른 설정없이 라우팅된다.</div>;
         <div>
